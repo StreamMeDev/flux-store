ed93a88e0810a384298871614bdd48eeda4facac
Wes <wes@wesleytodd.com>
2017-05-12 18:32:26 -0500

    knew I should have asked for a review, missed one export

--- src/bind-action-creators.js
+++ src/bind-action-creators.js
@@ -6,7 +6,7 @@ function bindActionCreator (actionCreator, dispatch) {
 	};
 }
 
-export function bindActionCreators (actionCreators, dispatch) {
+module.exports = function bindActionCreators (actionCreators, dispatch) {
 	if (typeof actionCreators === 'function') {
 		return bindActionCreator(actionCreators, dispatch);
 	}
@@ -15,4 +15,4 @@ export function bindActionCreators (actionCreators, dispatch) {
 		obj[key] = bindActionCreator(actionCreators[key], dispatch);
 		return obj;
 	}, {});
-}
+};
